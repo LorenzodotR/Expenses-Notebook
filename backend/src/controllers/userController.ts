11604e048acd73e8470b8de799c0f7006fe1f0f4
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-04-01 20:31:05 -0300

    UserController

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,56 @@
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        // const {name, sureName, passWord } 
+
+    } catch (error) {
+
+    }
+}
+
+export const getUser = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const recoverUserPassword = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const userLogin = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export const userLogout = async (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
---
+++ backend/src/middlewares/validationMiddleware.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+
+export const creatUserSchema = z.object({
+    email: z.string().email(),
+    birthDate: z.string().datetime(),
+    name: z.string(),
+    surname: z.string(),
+    password: z.string().min(6),
+    street: z.string(),
+    city: z.string(),
+    state: z.string(),
+    zipCode: z.string(),
+    country: z.string(),
+    bankId: z.string().uuid(),
+})
